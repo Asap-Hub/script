9c5a0e08cf2770f6ffdf38cacbd9878c6ac05472
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-05-29 20:50:57 +0000

    bases with f#

---
+++ funcDetail.fsx
@@ -0,0 +1,9 @@
+let square = (fun x -> x * x)
+square 5
+
+List.map(fun x -> x*x)[1..10]
+
+let com x y = if x > y then x-y else y-x
+
+com 2 3 * 2 + 1
+ 
---
+++ record.fsx
@@ -0,0 +1,11 @@
+ 
+
+type person = {
+    firstName: string;
+    secondName: string;
+    Age: int;
+}
+
+let person1 = {firstName = "Asap"; secondName = "Dasty"; Age =12}
+
+ 
